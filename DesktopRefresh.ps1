e9e4f27335c252fdc6630a374b5470cc9ff16e00
Tech Guy <43686905+TechScripts@users.noreply.github.com>
2021-10-22 22:32:09 +0530

    Update DesktopRefresh.ps1

--- DesktopRefresh.ps1
+++ DesktopRefresh.ps1
@@ -32,3 +32,23 @@ $WinShell.sendkeys("{SCROLLLOCK}")
 Start-Sleep -Seconds 120
 }
 
+######################################################################################
+#Third Script - It will keep your windows session alive and restart your machine at 7:00 AM.
+#Note: Make sure you start the script after 12:00 AM on any day or change the time as needed. 
+# If you run the script at 9 PM for example, (Get-Date) -ge $RebootTime condition results in true, as on that date, 9 PM, will be greater than 7 AM.
+#while ($true) runs forever and consumes more cpu. 
+
+for ($i = 1; $i -le 100000; $i++) {
+    $RebootTime = [datetime]"7:00"
+    if ((Get-Date) -ge $RebootTime) {
+        Stop-Computer -Force
+    }
+    write-host "Keep alive w/ scroll lock..." -foreground yellow
+    $WinShell = New-Object -com "Wscript.Shell"
+    $WinShell.sendkeys("{SCROLLLOCK}")
+    foreach ($one in (120..1)) {
+        Clear-Host
+        write-host Refreshing in $one seconds
+        Start-Sleep 1
+    }
+}
